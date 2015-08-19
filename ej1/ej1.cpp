c4164684ac3911431114d14a44010ad71a0da089
cosa65 <mfosco65@gmail.com>
2015-08-19 14:39:35 -0300

    cambie cosas

--- ej1/ej1.cpp
+++ ej1/ej1.cpp
@@ -11,24 +11,28 @@ using namespace std;
 #define ALCANZA ((int) 2)
 #define ALCANZA_SI_DESCARTO ((int) 1)
 #define NO_ALCANZA ((int) 0)
-																			//PARA CORRERLO : "./exj1 ej1/Tp1Ej1.in ej1/testeando.txt ej1/Tp1Ej1.o"
+																			//PARA CORRERLO : "./exj1 ej1/Tp1Ej1.in ej1/testeando.txt ej1/Tp1Ej1.out"
+
 ////////////////////////////////////////////////////AUXILIARES////////////////////////////////////////////////////
 void crearDists(int km, vector<int> ramales, int D[], int n){
-  vector<int>::reverse_iterator it = ramales.rbegin();
+  int r;
+  vector<int>::iterator it = ramales.begin();
   D[0] = *it;
   int j=1;
   while (j<n){
     int aux1 = *it;
     it++;
     int aux2 = *it;
-    D[j] = aux2 - aux1;
+    D[j] = aux1 - aux2;
     j++;
   }
 }
 
 int meAlcanzaCable(int D[],int km, int j, int i, tuple<int,int> tup1, tuple<int,int> tup2, tuple<int,int> *tupActual){
+  // int ciudAct=get<1>(*tupActual);
+  // int cabAct=get<0>(*tupActual);
   if(get<0>(*tupActual) + D[j] <= km) return ALCANZA;
-  if(get<0>(*tupActual) + D[j]-D[i] <= km) return ALCANZA_SI_DESCARTO;
+  if((get<0>(*tupActual) + D[j]-D[i] <= km)) return ALCANZA_SI_DESCARTO;
   return NO_ALCANZA;
 }
 
@@ -72,28 +76,26 @@ int ejUno(int km, vector<int> ramales) {
   tuple<int,int> *tupActual = &tup1;
   int n = ramales.size();
   int D[n];
-  //bool noPudeAgregar=false; //me dice si la ultima iteracion no pudo agregar cable ni descartando
 
-  crearDists(km, ramales, D, n);  //creo arreglo con las distancias, si la ciudades son [a,b,c,d,e,f] 
-  																//entonces D = [d(a,b), d(b,c), d(c,d), d(d,e), d(e,f)]
- 
-  //hay que chequear el caso en el que no se puedan conectar nigunas ciudades
-  //t1 = make_tuple(D[n], 2);
-  //t2 = make_tuple(D[n-1], 2);
+  crearDists(km, ramales, D, n);  	//creo arreglo con las distancias, si la ciudades son [a,b,c,d,e,f] 
+  									                //entonces D = [d(a,b), d(b,c), d(c,d), d(d,e), d(e,f)]
   get<0>(tup1)=0; get<1>(tup1)=0;
   get<0>(tup2)=0; get<1>(tup2)=0;					//tup[0]=cant de cable usado, tup[1]=cant de ciudades conectadas
   int j = n-2;
   int i = n-1;
 
+  int ciudAct,cabAct;
+
   while(j>=0){
-    int meAlcanza;
-    meAlcanza = meAlcanzaCable(D,km,j,i,tup1,tup2,tupActual);	//Devuelve un int que me indica si ALCANZA, ALCANZA_SI_DESCARTO o NO_ALCANZA
+    ciudAct=get<1>(*tupActual);
+    cabAct=get<0>(*tupActual);    
+    int meAlcanza = meAlcanzaCable(D,km,j,i,tup1,tup2,tupActual);	//Devuelve un int que me indica si ALCANZA, ALCANZA_SI_DESCARTO o NO_ALCANZA
     if(meAlcanza==ALCANZA){
       if(acaboDeEmpezar(tupActual)) get<1>(*tupActual)++;
       soloAgregarCiudad(D,km,j,i,&tup1,&tup2,tupActual);
       int debug=get<1>(*tupActual);
       j--;															//Muevo el indice que selecciona las ciudades a agregar
-    }else if(meAlcanza==ALCANZA_SI_DESCARTO){
+    }else if(meAlcanza==ALCANZA_SI_DESCARTO && !(acaboDeEmpezar(tupActual))){
     	//if(acaboDeEmpezar(tupActual)) get<1>(*tupActual)++;
       agregarCiudadRestando(D,km,j,i,&tup1,&tup2,tupActual);
       j--;															//Muevo el indice que selecciona las ciudades a agregar
@@ -153,7 +155,6 @@ int ejUno(int km, vector<int> ramales) {
   }*/
 
 int evaluarTests(string fileTestData, string fileTestResult, string fileTestWrite) {
-  bool open = true;
   vector<int> ramales;
   int i = 1;
   int km;
@@ -165,9 +166,8 @@ int evaluarTests(string fileTestData, string fileTestResult, string fileTestWrit
   // e instancie las variables necesarias para el problema
   // el kilometraje y el vector de ramales
 
-  while (open) {
-  	vector<int>::iterator it = ramales.begin();
-    getline (fileData, line);
+  while (getline (fileData, line)) {
+    vector<int>::iterator it = ramales.begin();
     km = atoi(line.c_str());
     // Primer dato a leer es el kilometraje
 
@@ -187,9 +187,6 @@ int evaluarTests(string fileTestData, string fileTestResult, string fileTestWrit
     // Almaceno el resultado del test i
     
     getline (fileResult, line);
-
-    if (fileResult.eof())
-      open = false;
 
     // Lei una linea del archivo de resultados
     // y pregunto si ya termine de evaluar todos los tests
