5eac7b0b3b59d9ed55f4c4a5102507666d8af80c
cosa65 <mfosco65@gmail.com>
2015-08-19 16:05:24 -0300

    cambie otra cosa

--- ej1/ej1.cpp
+++ ej1/ej1.cpp
@@ -5,7 +5,6 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <tuple>
 using namespace std;
 
 #define ALCANZA ((int) 2)
@@ -14,6 +13,12 @@ using namespace std;
 																			//PARA CORRERLO : "./exj1 ej1/Tp1Ej1.in ej1/testeando.txt ej1/Tp1Ej1.out"
 
 ////////////////////////////////////////////////////AUXILIARES////////////////////////////////////////////////////
+
+struct ramal{
+  int ciudades;
+  int cable;
+};
+
 void crearDists(int km, vector<int> ramales, int D[], int n){
   int r;
   vector<int>::iterator it = ramales.begin();
@@ -28,131 +33,92 @@ void crearDists(int km, vector<int> ramales, int D[], int n){
   }
 }
 
-int meAlcanzaCable(int D[],int km, int j, int i, tuple<int,int> tup1, tuple<int,int> tup2, tuple<int,int> *tupActual){
-  // int ciudAct=get<1>(*tupActual);
-  // int cabAct=get<0>(*tupActual);
-  if(get<0>(*tupActual) + D[j] <= km) return ALCANZA;
-  if((get<0>(*tupActual) + D[j]-D[i] <= km)) return ALCANZA_SI_DESCARTO;
+int meAlcanzaCable(int D[],int km, int j, int i, ramal acum1, ramal acum2, ramal *acumActual){
+  // int ciudAct=acumActual->ciudades;
+  // int cabAct=acumActual->cable;
+  if(acumActual->cable + D[j] <= km) return ALCANZA;
+  if((acumActual->cable + D[j]-D[i] <= km)) return ALCANZA_SI_DESCARTO;
   return NO_ALCANZA;
 }
 
-void soloAgregarCiudad(int D[], int km, int j, int i, tuple<int,int> *tup1, tuple<int,int> *tup2, tuple<int,int> *tupActual){
-  get<0>(*tupActual)+=D[j];
-  get<1>(*tupActual)++;
+void soloAgregarCiudad(int D[], int km, int j, int i, ramal *acum1, ramal *acum2, ramal *acumActual){
+  acumActual->cable+=D[j];
+  acumActual->ciudades++;
 }
 
-void agregarCiudadRestando(int D[], int km, int j, int i, tuple<int,int> *tup1, tuple<int,int> *tup2, tuple<int,int> *tupActual){
-  get<0>(*tupActual)+=(D[j] - D[i]);
+void agregarCiudadRestando(int D[], int km, int j, int i, ramal *acum1, ramal *acum2, ramal *acumActual){
+  acumActual->cable+=(D[j] - D[i]);
 }
 
-void elegirTuplaNueva(int D[], int km, int j, int i, tuple<int,int> *tup1, tuple<int,int> *tup2, tuple<int,int> **tupActualptr){
+void elegirTuplaNueva(int D[], int km, int j, int i, ramal *acum1, ramal *acum2, ramal **acumActualptr){
 	//Acá decido que tupla resetear y usar para el nuevo intervalo que voy crear, lo decido tomando la tupla que haya agarrado menos ciudades
-	if(get<1>(*tup1) >= get<1>(*tup2)){
-		*tupActualptr=tup2;
-		*tup2 = make_tuple(0,0);
+	if(acum1->ciudades >= acum2->ciudades){
+		*acumActualptr=acum2;
+		acum2->ciudades = 0;
+		acum2->cable = 0;
 	} else {
-		*tupActualptr=tup1;
-		*tup1 = make_tuple(0,0);
+		*acumActualptr=acum1;
+		acum1->ciudades = 0;
+		acum1->cable = 0;
 	}
 }
 
-bool acaboDeEmpezar(tuple<int,int> *tupActual){
+bool acaboDeEmpezar(ramal *acumActual){
 	//Indica si estoy empezando un nuevo intervalo con mi tupla actual (si todo vale 0)
-	return (get<0>(*tupActual)==0) && (get<1>(*tupActual)==0);
+	return (acumActual->cable==0) && (acumActual->ciudades==0);
 }
 
-int mayorCant(tuple<int,int> tup1, tuple<int,int> tup2){
-	if(get<1>(tup1)>=get<1>(tup2)){
-		int res=get<1>(tup1);
-		return get<1>(tup1);
+int mayorCant(ramal acum1, ramal acum2){
+	if(acum1.ciudades>=acum2.ciudades){
+		int res=acum1.ciudades;
+		return acum1.ciudades;
 	} 
-	return get<1>(tup2);
+	return acum2.ciudades;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int ejUno(int km, vector<int> ramales) {
-  tuple<int,int> tup1;
-  tuple<int,int> tup2;
-  tuple<int,int> *tupActual = &tup1;
+  ramal acum1;
+  ramal acum2;
+  ramal *acumActual = &acum1;
   int n = ramales.size();
   int D[n];
 
   crearDists(km, ramales, D, n);  	//creo arreglo con las distancias, si la ciudades son [a,b,c,d,e,f] 
   									                //entonces D = [d(a,b), d(b,c), d(c,d), d(d,e), d(e,f)]
-  get<0>(tup1)=0; get<1>(tup1)=0;
-  get<0>(tup2)=0; get<1>(tup2)=0;					//tup[0]=cant de cable usado, tup[1]=cant de ciudades conectadas
+  acum1.cable=0; acum1.ciudades=0;
+  acum2.cable=0; acum2.ciudades=0;					//tup[0]=cant de cable usado, tup[1]=cant de ciudades conectadas
   int j = n-2;
   int i = n-1;
 
   int ciudAct,cabAct;
 
   while(j>=0){
-    ciudAct=get<1>(*tupActual);
-    cabAct=get<0>(*tupActual);    
-    int meAlcanza = meAlcanzaCable(D,km,j,i,tup1,tup2,tupActual);	//Devuelve un int que me indica si ALCANZA, ALCANZA_SI_DESCARTO o NO_ALCANZA
+
+    ciudAct=acumActual->ciudades;
+    cabAct=acumActual->cable;    
+    int meAlcanza = meAlcanzaCable(D,km,j,i,acum1,acum2,acumActual);	//Devuelve un int que me indica si ALCANZA, ALCANZA_SI_DESCARTO o NO_ALCANZA
     if(meAlcanza==ALCANZA){
-      if(acaboDeEmpezar(tupActual)) get<1>(*tupActual)++;
-      soloAgregarCiudad(D,km,j,i,&tup1,&tup2,tupActual);
-      int debug=get<1>(*tupActual);
+      if(acaboDeEmpezar(acumActual)) acumActual->ciudades++;
+      soloAgregarCiudad(D,km,j,i,&acum1,&acum2,acumActual);
+      int debug=acumActual->ciudades;
       j--;															//Muevo el indice que selecciona las ciudades a agregar
-    }else if(meAlcanza==ALCANZA_SI_DESCARTO && !(acaboDeEmpezar(tupActual))){
-    	//if(acaboDeEmpezar(tupActual)) get<1>(*tupActual)++;
-      agregarCiudadRestando(D,km,j,i,&tup1,&tup2,tupActual);
+    }else if(meAlcanza==ALCANZA_SI_DESCARTO && !(acaboDeEmpezar(acumActual))){
+    	//if(acaboDeEmpezar(acumActual)) acumActual->ciudades++;
+      agregarCiudadRestando(D,km,j,i,&acum1,&acum2,acumActual);
       j--;															//Muevo el indice que selecciona las ciudades a agregar
       i--;															//Muevo el indice que selecciona las ciudades que finalizan el intervalo
     } else { //caso meAlcanza==NO_ALCANZA
       i=j; 															//el final de mi nuevo intervalo es en la ciudadque termina el intervalo actual
       j--;
-      elegirTuplaNueva(D,km,j,i,&tup1,&tup2,&tupActual);		//Modifica usotuplaNro para que decide que tupla flushear y cual conservar (se queda con la que mas ciudades encompase)
+      elegirTuplaNueva(D,km,j,i,&acum1,&acum2,&acumActual);		//Modifica usotuplaNro para que decide que tupla flushear y cual conservar (se queda con la que mas ciudades encompase)
     }
 
   }
 
-  int res = mayorCant(tup1,tup2);
+  int res = mayorCant(acum1,acum2);
   return res;
 }
-  /*while (j>0){
-    //el cable alcanza para agregar ciudad
-    if (get<0>(t1) + D[j] <= km){
-      get<0>(t1) = get<0>(t1) + D[j];
-      get<1>(t1) = get<1>(t1) + 1 ;
-    } else { //el cable alcanza si suelto la ultima ciudad
-      if (get<0>(t1) + D[j] - get<0>(t2) <= km){
-        get<0>(t1) = get<0>(t1) + D[j] - get<0>(t2);
-        //get<1>(t1) = get<1>(t1);
-        get<0>(t2) = D[j];
-      } else { //el cable no alcanza aunque suelte ciudad
-      
-      }  
-    }
-    j--;
-  }
-  return get<0>(t1);
-  */
-
-  //
-  //
-  // a = arreglo (numeros del texto)
- // j = tam(a) - 1 
-/*  c = crearTupla(0,0)
-  d = crearTupla(0,0)
-  // deberian empezar en dos o algo asi
-  while(j > 0) {
-    if (Pi_1(c) + (a[j]-a[j-1] < cable && acum2 + (a[j]-a[j-1]) < cable)){
-      // a = Pi_1(c) + (a[j]-a[j-1] < cable 
-      // b = acum2 + (a[j]-a[j-1]) < cable
-    c = (faumentar este numero que empieza en 2,anterior += a[j]-a[j-1])
-    } else {
-      if (!a && !b) {
-
-        else {
-          if (a && !b) {
-          
-          }
-        }
-      }
-    }
-  }*/
 
 int evaluarTests(string fileTestData, string fileTestResult, string fileTestWrite) {
   vector<int> ramales;
