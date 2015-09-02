2a7c99289def729f32c9d8ae9fdac5b51f091ad6
Jesica Vazquez <jevazquez@ws19.labo7>
2015-09-02 15:21:29 -0300

    tiempos ej2

--- ej2/mediana.cpp
+++ ej2/mediana.cpp
@@ -4,35 +4,62 @@
 #include <queue> //priority_queue
 #include <sstream> //istringstream
 #include <algorithm> //lower_bound
+#include<sys/time.h>
 // #include "string.h"
 using namespace std;
+
+timeval inicio, fin;
+
+void init_time()
+{
+  gettimeofday(&inicio, NULL);
+}
+
+double get_time()
+{
+  gettimeofday(&fin, NULL);
+  return (1000000*(fin.tv_sec-inicio.tv_sec) + (fin.tv_usec-inicio.tv_usec))/1000000.0;   
+}
 
 
 void mediana(string, stringstream&);
 int medianaSubarreglo(int, priority_queue<int>&, priority_queue<int, vector<int>, greater<int> >&);
 
 int main () {
-
-	string iline;
+	string iline; 
 	stringstream oss;
-	ifstream ifile ("test.in");
-	ofstream ofile ("test.out");
+	ifstream ifile ("test100000.in");
+	ofstream ofile ("testx.out");
+  ofstream tfile ("tiempo100000.txt");
+  float suma;
+  unsigned int i;
+  
+  for ( i = 0; i < 100; i++ )
+  {
+	  while ( getline (ifile,iline) )
+	  {
+        
+	    mediana(iline, oss);
+      
+	  	ofile << oss.rdbuf();
+	  	if (ifile.eof()) break;
+	  	ofile << "\n";
+	  }
 
-	while ( getline (ifile,iline) )
-	{
-		mediana(iline, oss);
-		ofile << oss.rdbuf();
-		if (ifile.eof()) break;
-		ofile << "\n";
-	}
-
-	ifile.close();
-
+	  ifile.close();
+    suma += get_time();
+ //   tfile << "Tiempo de ejecución: " << fixed << get_time() << endl;
+ //   tfile << "\n";
+  }
+  tfile << "Promedio: " << suma/(float)i << endl;
+  tfile.close();
 	return 0;
 }
 
+
 void mediana(string line, stringstream& oss)
 {
+  init_time();
 	priority_queue<int> menores;
 	priority_queue<int, vector<int>, greater<int> > mayores;
 	int prox, med;
@@ -42,6 +69,7 @@ void mediana(string line, stringstream& oss)
 	{
 		med = medianaSubarreglo(prox, menores, mayores);
 		oss << med;		
+  //  oss << get_time(); 
 		if (iss.eof()) break;
 		oss << " ";
 	}
@@ -93,5 +121,6 @@ int medianaSubarreglo(int prox, priority_queue<int>& menores, priority_queue<int
 		return mayores.top();
 	if (menores.size() == mayores.size())
 		return (menores.top()+mayores.top())/2;
+
 
 }
