f12d2bfae194d911067f91b4d70a9c5720cb25e7
cosa65 <mfosco65@gmail.com>
2015-09-02 17:45:17 -0300

    tiempos ej1

--- ej1/paGraficar/src/casos.py
+++ ej1/paGraficar/src/casos.py
@@ -1,12 +1,16 @@
 import sys
 import os
+import random
 #n=cantidad de cable
 #ms=cantidad de ciudades
-def printear(n, ms, name):
+def printear(n, ms, name, length):
+	random.seed()
 	f = open(name, 'w+')
 	f.write(n)
 	f.write(os.linesep)	
 	string = ''
+	a="mejor"
+	n=0
 	for m in xrange(1,ms+1):
 		#M = m+1
 		string += str(m) + ' '
@@ -14,5 +18,7 @@ def printear(n, ms, name):
 	f.write(os.linesep)
 	f.close()
 
+
+
 if __name__ == '__main__':
-	printear(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+	printear(sys.argv[1], int(sys.argv[2]), sys.argv[3], sys.argv[4])
